43afb8084795d9582fb8b7d2151f70a7232b3d8a
Rishikesh7713 <rishikeshpramoth@gmail.com>
2025-06-19 20:02:20 +0530

    I learned Python

---
+++ Python/Intro.py
@@ -0,0 +1,7 @@
+word1="Hello Rishikesh "
+word2="How are you?"
+print(word1+word2)
+
+text1="I am fine "
+text2="Come let's play"
+print(text1+text2)
---
+++ Python/Turtle-project.py
@@ -0,0 +1,61 @@
+import turtle
+turtle.Screen().bgcolor("orange")
+tt=turtle.Screen()
+tt.setup(400, 300)
+turtle.title("Welcome to Turtle Canvas")
+board=turtle.Turtle()
+board.forward(100)
+board.left(120)
+
+board.forward(100)
+board.left(120)
+
+board.forward(100)
+board.left(120)
+board.penup()
+
+board.goto(200, 100)
+
+board.pendown()
+
+board.forward(100)
+board.left(90)
+
+board.forward(100)
+board.left(90)
+
+board.forward(100)
+board.left(90)
+
+board.forward(100)
+board.left(90)
+
+board.penup()
+board.goto(-200, -100)
+board.pendown()
+
+board.forward(100)
+board.left(120)
+
+board.forward(100)
+board.left(120)
+
+board.forward(100)
+board.left(120)
+
+board.penup()
+
+board.right(120)
+board.forward(50)
+
+board.pendown()
+
+board.right(120)
+board.forward(100)
+
+board.right(120)
+board.forward(100)
+
+board.right(120)
+board.forward(100)
+turtle.done()
