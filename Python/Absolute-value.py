8c5708fdfcf562a235880cd4ccb4e5bda989e06b
Rishikesh7713 <rishikeshpramoth@gmail.com>
2025-06-30 19:50:30 +0530

    I made pallindrom

---
+++ Python/Absolute-value.py
@@ -0,0 +1,7 @@
+def absolute(num):
+    if num>=0:
+        return num
+    else:
+        return -num
+print("Absolute value of 89= ", absolute(89))
+print("Absolute value of -189= ", absolute(-189))
