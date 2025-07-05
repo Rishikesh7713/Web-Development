8bc8d455a7b3eb0d2effb4ee78b4a1c938fbe262
Rishikesh7713 <rishikeshpramoth@gmail.com>
2025-07-05 12:09:22 +0530

    I made my project

---
+++ Python/Method.py
@@ -0,0 +1,13 @@
+class parrot:
+ spiecies="bird"
+
+ def __init__(self, name, age):
+  self.name=name
+  self.age=age
+
+ def info(self):
+  print("Hello, I am Blu, I am ",blu.age, "and I am a ",blu.spiecies)
+
+  blu=parrot(blu, 12)
+  blu.info()
+
