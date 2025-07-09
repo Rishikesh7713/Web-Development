97f52a170039af7ca0988bd0dead19266d3e5764
Rishikesh7713 <rishikeshpramoth@gmail.com>
2025-07-09 12:25:26 +0530

    imd

--- Python/Face.py
+++ Python/Face.py
@@ -1,5 +1,5 @@
 import turtle
-class Face:
+class Face(turtle.Turtle):
     def __init__(self, xpos, ypos):
         self.size=90
         self.coord=(xpos,ypos)
@@ -23,6 +23,9 @@ class Face:
         self.penup()
         self.goto(self.coord)
 
+        self.setheading(0)
+
+
     def drawOutline(self):
         self.penup()
         self.forward(self.size)
@@ -30,3 +33,34 @@ class Face:
         self.pendown()
         self.circle(self.size)
         self.goHome()
+
+    def drawEye(self, turn):
+        self.penup()
+        self.left(turn)
+        self.forward(self.size/2)
+        self.pendown()
+        self.dot(self.size/10)
+        self.goHome()
+
+    def drawMouth(self):
+        self.penup()
+        self.right(135)
+        self.forward(self.size/1.7)
+        self.left(90)
+        self.pendown()
+        self.circle(self.size/1.7,90)
+        self.goHome()
+
+    def drawNose(self):
+        if self.noseSize == 'large':
+            self.dot(self.size/2, "grey")
+        elif self.noseSize == 'small':
+            self.dot(self.size/6, "grey")
+        else:
+            self.dot(self.size/4, "grey")
+            self.goHome()
+
+f1= Face(0, 0)
+f1.draw()
+    
+turtle.done
