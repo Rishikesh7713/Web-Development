fef3b5ae2f4b84082e222eb224cc8a633d1d4e3c
Rishikesh7713 <rishikeshpramoth@gmail.com>
2025-07-14 19:56:45 +0530

    I did

---
+++ Python/Currency.py
@@ -0,0 +1,11 @@
+def notes(a):
+    Q=[2000,500,200,100,50,20,10]
+    x=0
+    for i in range(7):
+        q=Q[i]
+        x=a//q
+        print("Notes of ",q,"=",x)
+        a=a%q
+
+amount= int(input("Enter an Amount"))
+notes(amount)
