95f642a0d4e88aed171b13caf4fb1ce2c0757956
Rishikesh7713 <rishikeshpramoth@gmail.com>
2025-08-15 18:57:38 +0530

    I made dice game

---
+++ Python/Dice.py
@@ -0,0 +1,30 @@
+import random
+ans=input("Do you want to play this game?")
+if ans=="yes".lower():
+   
+   def rolling(self):
+    self.random_choice=" "
+    opt=["1","2","3","4","5","6"]
+    choice=random.choice(list(opt))
+    if choice=="1":
+     random_choice="1"
+     print(random_choice)
+    if choice=="2":
+     random_choice="2"
+     print(random_choice)
+    if choice=="3":
+     random_choice="3"
+     print(random_choice)
+    if choice=="4":
+     random_choice="4"
+     print(random_choice)
+    if choice=="5":
+     random_choice="5"
+     print(random_choice)
+    if choice=="6":
+     random_choice="6"
+     print(random_choice) 
+else:
+   print("Thank you have a nice day")
+
+ 
