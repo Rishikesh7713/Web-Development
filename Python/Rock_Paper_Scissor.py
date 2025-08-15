5dc5dd304e97f902ebdbf17aa1dd0fac074f226c
Rishikesh7713 <rishikeshpramoth@gmail.com>
2025-08-15 18:23:46 +0530

    I did

--- Python/Rock_Paper_Scissor.py
+++ Python/Rock_Paper_Scissor.py
@@ -39,14 +39,93 @@ class button():
 
   def player(self):
    if self.rock_btn.clicked(30):
-    self.option="rock"
+    self.p_option="rock"
     self.screen.blit(self.choose_rbtn(120,200))
    elif self.paper_btn.clicked(30):
-    self.option="paper"
+    self.p_option="paper"
     self.screen.blit(self.choose_pbtn(120,200))
    else:
-    self.option="scissor"
+    self.p_option="scissor"
     self.screen.blit(self.choose_sbtn(120,200))
 
-    return self.option()
+    return self.p_option()
+   
+  def computer(self):
+   self.pc_random_choice=" "
+   option=["rock","paper","scissor"]
+   pcchoice=random.choice(list(option))
+   if pcchoice=="rock":
+    self.pc_random_choice="rock"
+    pcchoice=self.choose_rbtn
+   elif pcchoice=="paper":
+    self.pc_random_choice="paper"
+    pcchoice=self.choose_pbtn
+   else:
+    self.pc_random_choice="scissor"
+    pcchoice=self.choose_sbtn
+   pc_option=self.screen.blit(pcchoice,(600,200))
+   return pc_option()
+  
+  def pl_score_cache(self):
+   self.pl_score=0
+   self.pc_score=0
+   pl=self.p_option
+   pc=self.pc_random_choice
+   if pl=="rock"and pc=="paper" or pl=="paper"and pc=="scissor" or pl=="scissor"and pc=="rock":
+    self.pc_score+=1
+   elif pc==pl:
+    pass
+   else:
+    self.pl_score+=1
+   return self.pl_score()
+  
+  def pc_score_cache(self):
+   self.pl_score=0
+   self.pc_score=0
+   pl=self.p_option
+   pc=self.pc_random_choice
+   if pc=="rock"and pl=="paper" or pc=="paper"and pl=="scissor" or pc=="scissor"and pl=="rock":
+    self.pl_score+=1
+   elif pc==pl:
+    pass
+   else:
+    self.pc_score+=1
+   return self.pc_score()
+  
+  def image_reset(self):
+   self.screen.blit(self.text,(330,0))
+   self.text=self.font.render("",True,(0,0,0))
+   self.screen.blit(self.rbtn,(20,500))
+   self.screen.blit(self.pbtn,(330,500))
+   self.screen.blit(self.sbtn,(640,500))
+   pass
+  
+ def game_loop(self):
+   run=True
+   clock=pygame.time.Clock
+   Rps_game=Rpsgame()
+   while run:
+    pygame.display.update()
+    self.screen.blit(self.text,(330,0))
+    for event in pygame.event.get():
+     if event.type==pygame.quit:
+      run=False
+     if event.type==pygame.MOUSEBUTTONDOWN:
+      if self.rock_btn.clicked(30) or self.paper_btn.clicked(340) or self.scissor_btn.clicked(640):
+       Rps_game.image_reset()
+       Rps_game.player()
+       Rps_game.computer()
+       self.pl_score()+=Rps_game.pl_score_cache()
+       self.pc_score()+=Rps_game.pc_score_cache()
+       self.text=self.font.render(f"{self.pl_score} : {self.pc_score}", True, (255,255,255))
+    pygame.display.flip()
+    clock.tick(30)
+   pygame.quit()
+
+ if __name__=='__main__':
+   game=Rpsgame()
+   game.game_loop 
+
+    
+
    
