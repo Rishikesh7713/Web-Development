db49ed395dc2541525f28f188e94574d06a2abf8
Rishikesh7713 <rishikeshpramoth@gmail.com>
2025-09-22 20:01:04 +0530

    Add files via upload

---
+++ Python/Sprite.py
@@ -0,0 +1,71 @@
+import pygame
+import random
+
+
+spcolorch=pygame.USEREVENT+1
+bgcolorch=pygame.USEREVENT+2
+
+blue=pygame.Color('blue')
+green=pygame.Color('green')
+red=pygame.Color('red')
+
+magenta=pygame.Color('magenta')
+yellow=pygame.Color('yellow')
+pink=pygame.Color('pink')
+
+
+class sprite(pygame.sprite.Sprite):
+   def __init__(self,color,width,height):
+      super().__init__
+      self.image=pygame.Surface([width,height])
+      self.image.fill(color)
+      self.rect=self.image.get_rect()
+      self.velocity=[random.choice([-1,1]),random.choice([-1,1])]
+    
+   def update(self):
+      self.rect.move_ip(self.velocity)
+      boundary=False
+      if self.rect.left<=0 or self.rect.right>=500:
+         self.velocity[0]=-self.velocity[0]
+         boundary=False
+      if self.rect.top<=0 or self.rect.bottom>=400:
+         self.velocity[1]=-self.velocity[1]
+         boundary=False
+      if boundary:
+         pygame.event.post(pygame.event.Event(spcolorch))
+         pygame.event.post(pygame.event.Event(spcolorch))
+   def changecolor(self):
+      self.image.fill(random.choice([magenta,yellow,pink]))
+
+def changebgcolor(self):
+   global bgcolor
+   bgcolor=random.choice([blue,green,red])
+
+all_sp=pygame.sprite.Group()
+sp=sprite(red,20,30)
+sp.rect.x=random.randint(0,480)
+sp.rect.y=random.randint(0,370)
+all_sp.add(sp)
+
+screen=pygame.display.set_mode((500,500))
+pygame.display.set_caption("Sprites")
+
+bgcolor=blue
+screen.fill(bgcolor)
+clock=pygame.time.Clock()
+exit=False
+while not exit:
+  for event in pygame.event.get():
+    if event.type==pygame.QUIT:
+        exit=True
+    elif event.type==spcolorch:
+       sp.changecolor()
+    elif event.type==bgcolorch:
+       changebgcolor()
+
+  all_sp.update()
+  screen.fill(bgcolor)
+  all_sp.draw(screen)
+  pygame.display.flip()
+  clock.tick(30)
+pygame.quit()
