c1ff847f17dd67b7bceba9d4d644eaeaad5d1062
Rishikesh7713 <rishikeshpramoth@gmail.com>
2025-12-15 19:28:05 +0530

    Add files via upload

---
+++ Python/BitManipulation.py
@@ -0,0 +1,9 @@
+num1=101
+num2=11
+print(num1,num2)
+print("Num1 and Num2",num1 & num2)
+print("Num1 or Num2",num1 | num2)
+print("Num1 XOR Num2",num1 ^ num2)
+print("not Num1",~num1)
+print("Num1 Left Shift",num1 << 2)
+print("Num2 Right Shift",num2 >> 2)
