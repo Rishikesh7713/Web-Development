2d776a1bf7bd6814bb383b0db872f0580816dbdc
Rishikesh7713 <rishikeshpramoth@gmail.com>
2025-12-29 19:17:36 +0530

    Add files via upload

---
+++ Python/Power_of_4.py
@@ -0,0 +1,15 @@
+def powerof4(n):
+    count=0
+    if (n & (~(n & (n-1)))):
+        while (n>1):
+            n>>=1
+            count+=1
+        if (count%2==0):
+            return True
+        else:
+            return False
+n=int(input("Enter a Number : "))
+if (powerof4(n)):
+    print((n," is a power of 4"))
+else:
+    print(n," is not a power of 4")
